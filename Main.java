901b5057b8fff3f3de77ef81d8b3db7bafd3f31f
Etienne <etiennesene.ds@gmail.com>
2025-03-18 16:27:27 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
+
+git config --global user.name "SeneDS"
+git config --global user.email etiennesene.ds@gmail.com
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Etienne!");
